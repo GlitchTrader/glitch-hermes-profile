20cd7fa7c0e1081b29d506e303d158dfe053da1f
architectonic <ab@alanborger.com>
2026-08-06 02:56:17 -0300

    Hermes profile: stamp setup release 0.0.2.23

--- setup.ps1
+++ setup.ps1
@@ -233,7 +233,7 @@ finally {
 [ordered]@{
     schema_version = 'glitch.hermes.setup.v1'
     profile = $Profile
-    distribution_version = '0.0.2.22'
+    distribution_version = '0.0.2.23'
     gateway_supervised = $true
     plugin_enabled = $true
     jobs = @($directJob, $learningJob)
