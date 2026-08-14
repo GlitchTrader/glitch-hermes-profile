47c114345a8826f3b27346f6ab595e553db7042a
architectonic <ab@alanborger.com>
2026-08-14 03:19:51 -0300

    fix: align profile setup version

--- setup.ps1
+++ setup.ps1
@@ -233,7 +233,7 @@ finally {
 [ordered]@{
     schema_version = 'glitch.hermes.setup.v1'
     profile = $Profile
-    distribution_version = '0.0.2.44'
+    distribution_version = '0.0.2.45'
     gateway_supervised = $true
     plugin_enabled = $true
     jobs = @($directJob, $learningJob)
